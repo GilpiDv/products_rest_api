854c4b374b8dfcd5bf3adbbeb37ad837aa616203
GilpiDv <gilgonzalez981@gmail.com>
2025-06-05 19:19:44 -0600

    manage validation errors in a middleware

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { body } from "express-validator";
 import { createProduct } from "./handlers/product";
+import { handleInputErrors } from "./middleware";
 
 const router = Router(); 
 
@@ -10,13 +11,13 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', 
-    // Validation
     body('name')
         .notEmpty().withMessage('Name is required'),
     body('price')
         .notEmpty().withMessage('Price is required')
         .isNumeric().withMessage('Price must be a number')
         .custom(value => value > 0).withMessage('Invalid price'),
+    handleInputErrors,
     createProduct
 )
 
